bfac8f1741c0b4b0424324978c221728307c7e63
AntonKorznikov <korznikovantona@gmail.com>
2025-03-05 18:10:55 +0300

    add python script to find images that mostly activate neurons in trained sae

--- main.py
+++ main.py
@@ -15,22 +15,10 @@ import wandb
 
 
 cfg = get_default_cfg()
-cfg["model_name"] = "theaiinstitute/theia-tiny-patch16-224-cdiv"
-cfg["dataset_path"] = "data/imagenet_1k_resized_256"
-# cfg["dataset_path"] = "evanarlian/imagenet_1k_resized_256"
-cfg["sae_type"] = 'batch-topk'
-cfg["dict_size"] = 2**15
-cfg["top_k"] = 30
-cfg['wandb_project'] = 'theia-sae2.1'
-cfg['act_size'] = 768
 cfg['device'] = 'cuda:4'
-cfg["num_tokens"] = 1e8
-cfg["model_batch_size"] = 64
-cfg["num_batches_in_buffer"] = 500
-
 cfg = post_init_cfg(cfg)
-sae = BatchTopKSAE(cfg)
 
+sae = BatchTopKSAE(cfg)
 model = AutoModel.from_pretrained(cfg["model_name"], trust_remote_code=True).to(cfg['device'])
 activations_store = ImageActivationsStore(model, cfg)
 
