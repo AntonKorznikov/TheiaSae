15128bfa283c7d1b710758eca0f4aca1ee1e11db
Anton Korznikov <korznikovantona@gmail.com>
2025-03-12 17:13:43 +0300

    Update activation_store.py

--- activation_store.py
+++ activation_store.py
@@ -136,4 +136,4 @@ class ImageActivationsStore:
             self.dataloader_iter = iter(self.dataloader)
             batch = next(self.dataloader_iter)
             # Extract the tensor from the tuple (TensorDataset returns batches as tuples)
-        return batch
+        return batch[0]
